8bac629f9a2075b2a3c8bc9e1028de053462396a
Andy Bobro <andycyberries@gmail.com>
2023-10-15 01:02:47 +0300

    remove log

--- core/i18n.js
+++ core/i18n.js
@@ -1,7 +1,6 @@
 const { I18n } = require('i18n')
 
 module.exports = (req, res, next) => {
-    console.log(__CONFIG.translate);
     const i18n = new I18n(__CONFIG.translate)
     i18n.init(req, res)
     return next();
