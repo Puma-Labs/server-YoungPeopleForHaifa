b410c2941c8dafd72889637ef98498559c5a62a1
Andy Bobro <andycyberries@gmail.com>
2023-10-15 01:00:37 +0300

    en + he + new lang

--- core/i18n.js
+++ core/i18n.js
@@ -1,6 +1,7 @@
 const { I18n } = require('i18n')
 
 module.exports = (req, res, next) => {
+    console.log(__CONFIG.translate);
     const i18n = new I18n(__CONFIG.translate)
     i18n.init(req, res)
     return next();
